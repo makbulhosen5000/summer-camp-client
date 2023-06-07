6bb0a716e25c9b9f24311598eff9f632fb955431
mh akash <mhakash5000@gmail.com>
2023-06-07 08:50:39 +0600

    setup basic routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,24 +1,20 @@
+
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home/Home";
 import Main from "../Pages/Layouts/Main";
 import NotFound from "../Pages/NotFound/NotFound";
 
-const { createBrowserRouter } = require("react-router-dom");
-
 const router = createBrowserRouter([
-
-    {
-        path:'/',
-        element:<Main/>,
-        errorElement:<NotFound/>,
-        children:[
-            {
-              path:'/',
-              element:<Home/>
-            }
-        ]
-        
-    }
-
-])
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 export default router;
-
