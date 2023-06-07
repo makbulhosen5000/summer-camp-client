4697f6dafb36356a1af62ad9edeb38ec4a64fd36
mh akash <mhakash5000@gmail.com>
2023-06-07 08:56:48 +0600

    added not found page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import notFound from "../../assets/not-found.svg";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div>
-            not Found
-        </div>
-    );
+ 
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <img src={notFound} alt="" className="w-[25%]" />
+      <div className=" flex-col">
+        <h1 className="text-3xl pb-3">Page Not Found</h1>
+        <Link to="/" className="bg-yellow-500 p-2 rounded-md">
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
